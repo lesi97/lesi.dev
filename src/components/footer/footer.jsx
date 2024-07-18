56059b3cea9d477b010f9f3fd33f520f4c9893fb
lesi97 <lesi97@hotmail.co.uk>
2024-07-18 20:44:32 +0100

    https://x.com/t3dotgg/status/1813659951216357697

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -3,87 +3,18 @@ import "./footer-mobile.scss";
 import { YouTube, TikTok, Discord, XCorp, Instagram, Threads, Twitch, Email, BuyMeACoffee } from "../icons";
 
 export function Footer() {
-    const footerData = [
-        {
-            Title: "Twitch",
-            Image: <Twitch />,
-            Link: "https://twitch.tv/c_lesi",
-            Active: true,
-            ID: 0,
-        },
-        {
-            Title: "YouTube",
-            Image: <YouTube />,
-            Link: "https://www.youtube.com/@C_Lesi",
-            Active: true,
-            ID: 1,
-        },
-        {
-            Title: "TikTok",
-            Image: <TikTok />,
-            Link: "https://www.tiktok.com/@c_lesi",
-            Active: true,
-            ID: 2,
-        },
-        {
-            Title: "Discord",
-            Image: <Discord />,
-            Link: "https://discord.gg/RUDhkXT",
-            Active: true,
-            ID: 3,
-        },
-        {
-            Title: "X (Formerly Twitter)",
-            Image: <XCorp />,
-            Link: "https://x.com/Chris_Lesi",
-            Active: true,
-            ID: 4,
-        },
-        {
-            Title: "Instagram",
-            Image: <Instagram />,
-            Link: "https://www.instagram.com/christian_lesi",
-            Active: true,
-            ID: 5,
-        },
-        {
-            Title: "Threads",
-            Image: <Threads />,
-            Link: "https://www.threads.net/christian_lesi",
-            Active: false,
-            ID: 6,
-        },
-        {
-            Title: "Buy Me A Coffee",
-            Image: <BuyMeACoffee />,
-            Link: "https://www.buymeacoffee.com/lesi",
-            Active: true,
-            ID: 7,
-        },
-        {
-            Title: "Email",
-            Image: <Email />,
-            Link: "mailto:chris.lesi001@gmail.com",
-            Active: true,
-            ID: 8,
-        },
-    ];
-
     return (
         <footer>
             <ul>
-                {footerData.map((socials) => {
-                    if (socials.Active) {
-                        return (
-                            <li key={socials.ID}>
-                                <a href={socials.Link} target="_blank" rel="noreferrer">
-                                    {socials.Image}
-                                </a>
-                            </li>
-                        );
-                    }
-                    return null;
-                })}
+                <li><a href="https://twitch.tv/c_lesi" target="_blank" rel="noreferrer"><Twitch /></a></li>
+                <li><a href="https://www.youtube.com/@C_Lesi" target="_blank" rel="noreferrer"><YouTube /></a></li>
+                <li><a href="https://www.tiktok.com/@c_lesi" target="_blank" rel="noreferrer"><TikTok /></a></li>
+                <li><a href="https://discord.gg/RUDhkXT" target="_blank" rel="noreferrer"><Discord /></a></li>
+                <li><a href="https://x.com/Chris_Lesi" target="_blank" rel="noreferrer"><XCorp /></a></li>
+                <li><a href="https://www.instagram.com/christian_lesi" target="_blank" rel="noreferrer"><Instagram /></a></li>
+                {/* <li><a href="https://www.threads.net/christian_lesi" target="_blank" rel="noreferrer"><Threads /></a></li> */}
+                <li><a href="https://www.buymeacoffee.com/lesi" target="_blank" rel="noreferrer"><BuyMeACoffee /></a></li>
+                <li><a href="mailto:chris.lesi001@gmail.com" target="_blank" rel="noreferrer"><Email /></a></li>
             </ul>
         </footer>
     );
--- src/components/nav/nav.jsx
+++ src/components/nav/nav.jsx
@@ -3,190 +3,38 @@ import "./nav-mobile.scss";
 import { Link } from "react-router-dom";
 import { BurgerMenu, Settings } from "../icons";
 import emote56 from "../../assets/images/emote56.webp";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 
-
-// function mobileMenu(e) {
-//     e.preventDefault();
-//     const navList = document.getElementById("myLinks");
-//     navList.classList.toggle("myLinks");
-// }
-
-function mobileMenu() {
-    const nav = document.getElementById("myLinks");
-    if (nav.style.display === "block") {
-        nav.style.display = "none";
-    } else {
-        nav.style.display = "block";
-    }
-}
-
-const NavButton = (props) => {
-
-    function handleClick() {
-        props.setPage(props.link);
-        props.mobile ? mobileMenu() : null;
-    }
-
-
-    if (props.active && props.title !== "Mobile Menu") {
-        return (
-            <>
-                <li key={props.id}
-                    className={`${props.mobileHide ? "mobileHide" : ""}${props.desktopHide ? " desktopHide" : ""}`}
-                    onClick={handleClick}
-                    id={props.title === "Mobile Menu" ? "mobileMenu" : ""}
-                    tabIndex="-1">
-                    <Link to={props.link}
-                        className={`${props.link === props.activePage ? " active" : ""}`}>
-                        {props.title !== "Settings" && props.title}
-                        {props.title === "Settings" && !props.mobile && (
-                            <span className="mobileHide">
-                                <Settings />
-                            </span>
-                        )}
-                        {props.title === "Settings" && props.mobile && (
-                            <span className="mobileShow">Settings</span>
-                        )}
-                    </Link>
-                </li>
-
-            </>
-        );
-    }
-    return null;
-};
 
 export function Nav() {
     const [currentPage, setCurrentPage] = useState(window.location.pathname);
-    const [isMobile, setIsMobile] = useState(window.innerWidth < 767);
 
-    function setPage(e) {
-        setCurrentPage(e)
+    const isMobile = window.innerWidth < 767;
+
+    function handleClick(e) {
+        setCurrentPage(e.target.attributes[1].nodeValue)
+        isMobile ? mobileMenu() : null;
     }
 
-    const navbarData = [
-        {
-            ID: 0,
-            Title: "Home",
-            Image: emote56,
-            Link: "/",
-            MobileHide: false,
-            DesktopHide: false,
-            Active: true,
-        },
-        {
-            ID: 1,
-            Title: "Aspect Ratio Calculator",
-            Link: "/aspect-ratio-calculator",
-            MobileHide: false,
-            DesktopHide: false,
-            Active: true,
-        },
-        {
-            ID: 2,
-            Title: "PDF Converter",
-            Link: "/pdf-to-png",
-            MobileHide: true,
-            DesktopHide: false,
-            Active: true,
-        },
-        {
-            ID: 3,
-            Title: "Icon Converter",
-            Link: "/ico-converter",
-            MobileHide: true,
-            DesktopHide: false,
-            Active: true,
-        },
-        {
-            ID: 4,
-            Title: "Password Generator",
-            Link: "/password-generator",
-            MobileHide: false,
-            DesktopHide: false,
-            Active: true,
-        },
-        {
-            ID: 5,
-            Title: "SVG Converter",
-            Link: "/svg-converter",
-            MobileHide: true,
-            DesktopHide: false,
-            Active: false,
-        },
-        {
-            ID: 6,
-            Title: "Weight Converter",
-            Link: "/weight-converter",
-            MobileHide: false,
-            DesktopHide: false,
-            Active: true,
-        },
-        {
-            ID: 7,
-            Title: "Video Editor",
-            Link: "/video-editor",
-            MobileHide: false,
-            DesktopHide: false,
-            Active: false,
-        },
-        {
-            ID: 8,
-            Title: "Video To MP3",
-            Link: "/video-to-mp3",
-            MobileHide: false,
-            DesktopHide: false,
-            Active: true,
-        },
-        {
-            ID: 9,
-            Title: "Video Cropper",
-            Link: "/video-cropper",
-            MobileHide: false,
-            DesktopHide: false,
-            Active: true,
-        },
-        {
-            ID: 10,
-            Title: "Minifier",
-            Link: "/minifier",
-            MobileHide: false,
-            DesktopHide: false,
-            Active: true,
-        },
-        {
-            ID: 11,
-            Title: "Store",
-            Link: "https://www.etsy.com/uk/shop/ShopLesii",
-            MobileHide: false,
-            DesktopHide: false,
-            Active: false,
-        },
-        {
-            ID: 12,
-            Title: "Mobile Menu",
-            Link: "",
-            MobileHide: false,
-            DesktopHide: false,
-            Active: true,
-        },
-        {
-            ID: 13,
-            Title: "Settings",
-            Link: "/settings",
-            MobileHide: false,
-            DesktopHide: false,
-            Active: true,
-        },
-    ];
+    function handleButtonCLick(e) {
+        if (e.keyCode === 13) return handleClick(e);
+    }
+
+    function mobileMenu() {
+        const nav = document.getElementById("myLinks");
+        if (nav.style.display === "block") {
+            nav.style.display = "none";
+        } else {
+            nav.style.display = "block";
+        }
+    }
 
     return (
         <header>
             <nav>
-                <Link id="homeLink" to={navbarData[0].Link}>
+                <Link id="homeLink" to="/">
                     <img
-                        src={navbarData[0].Image}
+                        src={emote56}
                         height={47}
                         width={47}
                         alt="Lesi"
@@ -194,28 +42,113 @@ export function Nav() {
                 </Link>
 
                 <ul id="myLinks" className="myLinks">
-                    {navbarData.map((nav) => {
-                        return (
-                            <NavButton
-                                key={nav.ID}
-                                id={nav.ID}
-                                title={nav.Title}
-                                link={nav.Link}
-                                mobileHide={nav.MobileHide}
-                                desktopHide={nav.DesktopHide}
-                                active={nav.Active}
-                                activePage={currentPage}
-                                setPage={e => setPage(e)}
-                                mobile={isMobile}
-                            />
-                        );
-                    })}
+                    <li>
+                        <Link to="/"
+                            className={`${"/" === currentPage ? " active" : ""}`}
+                            onClick={(e) => handleClick(e)}
+                            onKeyDown={(e) => handleButtonCLick(e)}
+                            tabIndex={0}>
+                            Home
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/aspect-ratio-calculator"
+                            className={`${"/aspect-ratio-calculator" === currentPage ? " active" : ""}`}
+                            onClick={(e) => handleClick(e)}
+                            onKeyDown={(e) => handleButtonCLick(e)}>
+                            Aspect Ratio Calculator
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/pdf-to-png"
+                            className={`${"/pdf-to-png" === currentPage ? " active" : ""}`}
+                            onClick={(e) => handleClick(e)}
+                            onKeyDown={(e) => handleButtonCLick(e)}>
+                            PDF Converter
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/ico-converter"
+                            className={`${"/ico-converter" === currentPage ? " active" : ""}`}
+                            onClick={(e) => handleClick(e)}
+                            onKeyDown={(e) => handleButtonCLick(e)}>
+                            Icon Converter
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/password-generator"
+                            className={`${"/password-generator" === currentPage ? " active" : ""}`}
+                            onClick={(e) => handleClick(e)}
+                            onKeyDown={(e) => handleButtonCLick(e)}>
+                            Password Generator
+                        </Link>
+                    </li>
+                    {/* <li>
+                        <Link to="/svg-converter"
+                            className={`${"/svg-converter" === currentPage ? " active" : ""}`}>
+                            SVG Converter
+                        </Link>
+                    </li> */}
+                    <li>
+                        <Link to="/weight-converter"
+                            className={`${"/weight-converter" === currentPage ? " active" : ""}`}
+                            onClick={(e) => handleClick(e)}
+                            onKeyDown={(e) => handleButtonCLick(e)}>
+                            Weight Converter
+                        </Link>
+                    </li>
+                    {/* <li>
+                        <Link to="/video-editor"
+                            className={`${"/video-editor" === currentPage ? " active" : ""}`}>
+                            Video Editor
+                        </Link>
+                    </li> */}
+                    <li>
+                        <Link to="/video-to-mp3"
+                            className={`${"/video-to-mp3" === currentPage ? " active" : ""}`}
+                            onClick={(e) => handleClick(e)}
+                            onKeyDown={(e) => handleButtonCLick(e)}>
+                            Video To MP3
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/video-cropper"
+                            className={`${"/video-cropper" === currentPage ? " active" : ""}`}
+                            onClick={(e) => handleClick(e)}
+                            onKeyDown={(e) => handleButtonCLick(e)}>
+                            Video Cropper
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/minifier"
+                            className={`${"/minifier" === currentPage ? " active" : ""}`}
+                            onClick={(e) => handleClick(e)}
+                            onKeyDown={(e) => handleButtonCLick(e)}>
+                            Minifier
+                        </Link>
+                    </li>
+                    {/* <li>
+                        <a href="https://www.etsy.com/uk/shop/ShopLesii" target="blank" rel="noreferrer">
+                            Store
+                        </a>
+                    </li> */}
+                    <li>
+                        <Link to="/settings"
+                            className={`${"/settings" === currentPage ? " active" : ""}`}
+                            onClick={(e) => handleClick(e)}
+                            onKeyDown={(e) => handleButtonCLick(e)}>
+                            {!isMobile
+                                ? <Settings />
+                                : "Settings"
+                            }
+                        </Link>
+                    </li>
                 </ul>
 
                 {isMobile && (
-                    <div className="burger-menu" onClick={mobileMenu}><BurgerMenu /></div>
+                    <button className="burger-menu" onClick={mobileMenu}><BurgerMenu /></button>
                 )}
             </nav>
-        </header>
+        </header >
     );
 }
