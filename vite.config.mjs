9389df92ba77646ff0bac11413a6e46bee474043
lesi97 <lesi97@hotmail.co.uk>
2024-09-10 14:37:43 +0100

    changes to vite config

--- vite.config.mjs
+++ vite.config.mjs
@@ -17,6 +17,7 @@ export default defineConfig({
     base: './',
     build: {
         // outDir: '../build/',
-        outDir: 'dist/'
+        outDir: '../dist/',
+        emptyOutDir: true,
     },
 });
