d37c4a0cfef0f6660103988585550881ed313036
lesi97 <lesi97@hotmail.co.uk>
2026-01-06 21:51:00 +0000

    fixed build error

--- apps/web/src/components/layout/ffmpegLayout.tsx
+++ apps/web/src/components/layout/ffmpegLayout.tsx
@@ -22,7 +22,7 @@ export function FfmpegLayout({ hasAudio = false }: { hasAudio?: boolean }) {
 
 function MainContent() {
     return (
-        <main className='relative top-8 mb-8 flex h-fit w-11/12 justify-center rounded-lg bg-base-100 px-8 py-8 shadow peer-[data-state="open"]:opacity-0 2xl:w-50% 2xl:min-w-50%'>
+        <main className='relative top-8 mb-8 flex h-fit w-11/12 justify-center rounded-lg bg-base-100 px-8 py-8 shadow 2xl:w-50% 2xl:min-w-50%'>
             <div className='flex w-11/12 flex-col gap-4'>
                 <Outlet />
             </div>
