5982237b2a6ca3eb3441c4b2a00a79fae9de62b3
lesi97 <lesi97@hotmail.co.uk>
2026-02-27 19:44:14 +0000

    feat(api): add Steam known-name mappings for re9 and marathon beta

--- apps/api/internal/domains/steam/internal/store/get_known_game_id_by_name.go
+++ apps/api/internal/domains/steam/internal/store/get_known_game_id_by_name.go
@@ -3,6 +3,9 @@ package store
 func GetKnownGameIDByName(normalisedGameName string) (string, bool) {
 	knownGameIDs := map[string]string{
 		"rocket league": "252950",
+		"resident evil 9": "3764200",
+		"re9": "3764200",
+		"marathon beta": "4254230",
 	}
 
 	gameID, ok := knownGameIDs[normalisedGameName]
