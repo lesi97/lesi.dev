c2ff55ca50cb844147e4d64e11ae899a9ef7fac3
lesi97 <lesi97@hotmail.co.uk>
2026-02-28 17:11:44 +0000

    fix: retry ValidateAndFetchApiDetails every 30s with runtime logs

--- apps/api/internal/db/validate_and_get_api_records.go
+++ apps/api/internal/db/validate_and_get_api_records.go
@@ -3,6 +3,7 @@ package db
 import (
 	"context"
 	"fmt"
+	"time"
 
 	"github.com/lesi97/lesi.dev/internal/utils"
 )
@@ -19,53 +20,64 @@ func (db *DB) ValidateAndFetchApiDetails(data ValidateApiDetailsArgs) (*ApiDetai
 	logger := data.Logger
 	discordUsername := data.DiscordUsername
 	authUrl := data.AuthUrl
+	for {
+		fmt.Printf("ValidateAndFetchApiDetails running for %v\n", application)
 
-	apiDetails, err := db.FetchApiDetails(context.Background(), application, logger)
-	if err != nil || apiDetails == nil {
-		message := fmt.Sprintf("FATAL: ERROR GETTING %v API DETAILS %v\n", application, err)
-		logger.SendDiscordNotification(utils.SendDiscordNotificationArgs{
-			Content:  message,
-			Username: discordUsername,
-			Title:    fmt.Sprintf("%v FATAL", application),
-		})
-		return nil, fmt.Errorf("%s", message)
-	}
-
-	noRefreshToken := apiDetails.RefreshToken == nil
-	if noRefreshToken {
-		message := fmt.Sprintf("No %v Refresh Token Found", application)
-		logger.Error(message)
-		return nil, fmt.Errorf("%s", message)
-	}
-
-	if apiDetails.RefreshTokenExpiry == nil {
-		message := fmt.Sprintf("No %v Refresh Token Expiry Found", application)
-		logger.Error(message)
-		return nil, fmt.Errorf("%s", message)
-	}
-
-	expired := utils.IsRefreshTokenExpired(*apiDetails.RefreshTokenExpiry)
-	if expired {
-		newAuthData, err := utils.RefreshToken(context.Background(), application, *apiDetails.ClientID, *apiDetails.ClientSecret, *apiDetails.RefreshToken, authUrl)
-		if err != nil {
-			fmt.Printf("NEW AUTH DATA ERR: %v\n", err)
-			return nil, err
+		apiDetails, err := db.FetchApiDetails(context.Background(), application, logger)
+		if err != nil || apiDetails == nil {
+			message := fmt.Sprintf("FATAL: ERROR GETTING %v API DETAILS %v\n", application, err)
+			logger.SendDiscordNotification(utils.SendDiscordNotificationArgs{
+				Content:  message,
+				Username: discordUsername,
+				Title:    fmt.Sprintf("%v FATAL", application),
+			})
+			fmt.Printf("%sRetrying %v in 30 seconds\n", message, application)
+			time.Sleep(30 * time.Second)
+			continue
 		}
-		apiDetails.AccessToken = &newAuthData.AccessToken
-		apiDetails.RefreshToken = &newAuthData.RefreshToken
-		apiDetails.RefreshTokenExpiry = newAuthData.RefreshTokenExpiry
-		_ = db.UpdateApiDetails(
-			context.Background(),
-			application,
-			nil,
-			nil,
-			apiDetails.AccessToken,
-			apiDetails.RefreshToken,
-			apiDetails.RefreshTokenExpiry,
-			nil,
-			nil,
-			logger,
-		)
+
+		noRefreshToken := apiDetails.RefreshToken == nil
+		if noRefreshToken {
+			message := fmt.Sprintf("No %v Refresh Token Found", application)
+			logger.Error(message)
+			fmt.Printf("%s. Retrying in 30 seconds\n", message)
+			time.Sleep(30 * time.Second)
+			continue
+		}
+
+		if apiDetails.RefreshTokenExpiry == nil {
+			message := fmt.Sprintf("No %v Refresh Token Expiry Found", application)
+			logger.Error(message)
+			fmt.Printf("%s. Retrying in 30 seconds\n", message)
+			time.Sleep(30 * time.Second)
+			continue
+		}
+
+		expired := utils.IsRefreshTokenExpired(*apiDetails.RefreshTokenExpiry)
+		if expired {
+			newAuthData, err := utils.RefreshToken(context.Background(), application, *apiDetails.ClientID, *apiDetails.ClientSecret, *apiDetails.RefreshToken, authUrl)
+			if err != nil {
+				fmt.Printf("NEW AUTH DATA ERR: %v\n", err)
+				fmt.Printf("Retrying %v in 30 seconds\n", application)
+				time.Sleep(30 * time.Second)
+				continue
+			}
+			apiDetails.AccessToken = &newAuthData.AccessToken
+			apiDetails.RefreshToken = &newAuthData.RefreshToken
+			apiDetails.RefreshTokenExpiry = newAuthData.RefreshTokenExpiry
+			_ = db.UpdateApiDetails(
+				context.Background(),
+				application,
+				nil,
+				nil,
+				apiDetails.AccessToken,
+				apiDetails.RefreshToken,
+				apiDetails.RefreshTokenExpiry,
+				nil,
+				nil,
+				logger,
+			)
+		}
+		return apiDetails, nil
 	}
-	return apiDetails, nil
 }
