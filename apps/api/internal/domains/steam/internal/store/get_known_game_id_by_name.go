08255d6dc9ce2bafff095b1353cc2f8a265346aa
lesi97 <lesi97@hotmail.co.uk>
2026-04-22 12:19:15 +0100

    feat: added typo mapping for game

--- apps/api/internal/domains/steam/internal/store/get_known_game_id_by_name.go
+++ apps/api/internal/domains/steam/internal/store/get_known_game_id_by_name.go
@@ -2,19 +2,20 @@ package store
 
 func GetKnownGameIDByName(normalisedGameName string) (string, bool) {
 	knownGameIDs := map[string]string{
-		"rocket league":         "252950",
-		"resident evil 9":       "3764200",
-		"re9":                   "3764200",
-		"marathon beta":         "4254230",
-		"deadlock":              "1422450",
-		"ghost of tushima":      "2215430",
-		"call of duty vanguard": "1985820",
-		"cod vanguard":          "1985820",
-		"rainbow six siege":     "359550",
-		"rainbow six":           "359550",
-		"r6":                    "359550",
-		"the first descendent":  "2074920",
-		"first descendent":      "2074920",
+		"rocket league":                 "252950",
+		"resident evil 9":               "3764200",
+		"re9":                           "3764200",
+		"marathon beta":                 "4254230",
+		"deadlock":                      "1422450",
+		"ghost of tushima":              "2215430",
+		"call of duty vanguard":         "1985820",
+		"cod vanguard":                  "1985820",
+		"rainbow six siege":             "359550",
+		"rainbow six":                   "359550",
+		"r6":                            "359550",
+		"the first descendent":          "2074920",
+		"first descendent":              "2074920",
+		"hello kitty island adventures": "2495100",
 	}
 
 	gameID, ok := knownGameIDs[normalisedGameName]
