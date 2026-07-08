47e18587c7ecb580b46d109f4654f2da83a09236
lesi97 <lesi97@hotmail.co.uk>
2026-07-08 16:05:00 +0100

    bug: remove quote marks from text response as it causes an issue on nightbot messages

--- apps/api/internal/domains/bungie/handler/text_response.go
+++ apps/api/internal/domains/bungie/handler/text_response.go
@@ -2,6 +2,7 @@ package handler
 
 import (
 	"net/http"
+	"strings"
 
 	"github.com/lesi97/lesi.dev/internal/utils"
 )
@@ -9,6 +10,15 @@ import (
 const twitchMessageCharacterLimit = 400
 
 func textResponse(w http.ResponseWriter, status int, message string) {
+	message = sanitiseTextResponse(message)
 	utils.TruncateString(&message, twitchMessageCharacterLimit)
 	utils.TextResponse(w, status, message)
+}
+
+func sanitiseTextResponse(message string) string {
+	if !strings.Contains(message, "\"") {
+		return message
+	}
+
+	return strings.ReplaceAll(message, "\"", "")
 }
